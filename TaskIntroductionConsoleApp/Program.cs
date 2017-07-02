0b37dac13057656ba2c1b69649374c4404667a85
wudifeixue <wudifeixue@users.noreply.github.com>
2017-07-02 17:17:01 -0600

    Explore few different ways of writing Task and add follow up too

--- TaskIntroductionConsoleApp/Program.cs
+++ TaskIntroductionConsoleApp/Program.cs
@@ -14,11 +14,25 @@ namespace TaskIntroductionConsoleApp
         {
             Stopwatch sw = Stopwatch.StartNew();
 
+            // The following also works, just a different way to write it
+            // Task<int> tsk = new Task<int>(BookCar);
+            // tsk.Start();
+
             Task<int> carTask = Task.Factory.StartNew<int>(BookCar);
             Task<int> hotelTask = Task.Factory.StartNew<int>(BookHotel);
             Task<int> planeTask = Task.Factory.StartNew<int>(BookPlane);
 
-            Task.WaitAll(carTask, hotelTask, planeTask);
+            // Add a follow up hotel Task after the three task
+            Task hotelFollowupTask = hotelTask.ContinueWith(
+                taskPrev => Console.WriteLine("Add view note for booking {0}", taskPrev.Result));
+
+            
+            hotelFollowupTask.Wait();
+
+            // Task.WaitAll(carTask, hotelTask, planeTask);
+            // Or use this WriteLine they works just fine
+            Console.WriteLine("Finished booking carId = {0}, hotelId = {1}, planeId = {2}",
+                carTask.Result, hotelTask.Result, planeTask.Result);
 
             Console.WriteLine("Finished in {0} seconds.", sw.ElapsedMilliseconds / 1000.0);
         }
