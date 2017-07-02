a7cd36a5eac7885374877908a16f5e51ba5932c3
wudifeixue <wudifeixue@users.noreply.github.com>
2017-07-02 16:24:57 -0600

    Write a sychrounous booking demo

--- TaskIntroductionConsoleApp/Program.cs
+++ TaskIntroductionConsoleApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskIntroductionConsoleApp
@@ -10,6 +12,39 @@ namespace TaskIntroductionConsoleApp
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            int carId = BookCar();
+            int hotelId = BookHotel();
+            int planeId = BookPlane();
+
+            Console.WriteLine("Finished in {0} seconds.", sw.ElapsedMilliseconds / 1000.0);
+        }
+
+        static Random random = new Random();
+
+        private static int BookPlane()
+        {
+            Console.WriteLine("Booking plane...");
+            Thread.Sleep(4000);
+            Console.WriteLine("Plane booked.");
+            return random.Next(100);
+        }
+
+        private static int BookHotel()
+        {
+            Console.WriteLine("Booking hotel...");
+            Thread.Sleep(6000);
+            Console.WriteLine("Hotel booked.");
+            return random.Next(100);
+        }
+
+        private static int BookCar()
+        {
+            Console.WriteLine("Booking car...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Car booked.");
+            return random.Next(100);
         }
     }
 }
